d1579a12545f76be7aa9453db42122cb75ad5fa5
Joe Cha <josephmcha@gmail.com>
2017-07-07 21:34:39 -0400

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+count = 0
+list = []
+listcount = 0
+while count < collection.length do
+  if yield(collection[count])
+  list[listcount] = collection[count]
+  count += 1
+  listcount += 1
+else
+  count += 1
+end
+end
+list  # your code here!
 end
